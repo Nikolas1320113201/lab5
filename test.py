9d2d26a163e99caa1e4a215530a05206db59a2f6
Nikolas <58080745+Nikolas1320113201@users.noreply.github.com>
2020-12-18 12:03:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,9 @@ import unittest
 from lab2 import Calculator
 
 class TestCalculator(unittest.TestCase):
-  test = Calculator(2)
   
   def n_chlen_test(self):
-    self.assertEqual(self.test.n_chlen(3), 7)
+    self.assertEqual(self.n_chlen(3), 7)
 
 if __name__ == "__main__":
   import xmlrunner
