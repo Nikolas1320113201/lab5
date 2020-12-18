e3b2e51ffc39dd22b89ba3de74fbcb233fded49d
Nikolas <58080745+Nikolas1320113201@users.noreply.github.com>
2020-12-18 11:40:27 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,27 +1,7 @@
-class length:
-    def __init__(self, n):
-        self.n = n
+class Calculator:
 
-    def __add__(self, x):
-        return self.n + x
+  def __init__(self):
+    pass
 
-    def __sub__(self, x):
-        if x==str:
-            raise TypeError
-        if self.n > x:
-            return self.n - x
-        else:
-            return x - self.n
-
-    def __mul__(self, x):
-         if not isinstance(x, int):
-            raise TypeError
-         else:
-            return self.n * x
-
-
-l = length(8.90)
-print(l.__add__(3))
-print(l.__sub__(9))
-print(l.__sub__(8))
-print(l.__mul__(2))
+  def n_chlen(self, n):
+    return 3+2*n
