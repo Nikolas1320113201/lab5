c52b8e8feafb3c12adaf2310f30a9bcc1f2714d6
Nikolas <58080745+Nikolas1320113201@users.noreply.github.com>
2020-12-18 12:08:11 +0200

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,6 +1,7 @@
-class Calculator:
-
-  def __init__(self, n):
+class length:
+    def __init__(self, n):
         self.n = n
-  def n_chlen(self):
-    return 3+self.n * 2
+
+    def __add__(self, x):
+        return self.n*2 + 3
+
